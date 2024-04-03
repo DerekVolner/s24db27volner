b46237ddeeebd0fc64609c533ad049f45bd3a793
Derek Volner <89471429+DerekVolner@users.noreply.github.com>
2024-04-03 15:52:00 -0500

    Part 7 Completed

--- controllers/rhinoSchema.js
+++ controllers/rhinoSchema.js
@@ -16,10 +16,27 @@ exports.rhinoSchema_list = async function(req, res) {
 exports.rhinoSchema_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: Rhino detail: ' + req.params.id);
 };
+
 // Handle Rhino create on POST.
-exports.rhinoSchema_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: Rhino create POST');
-};
+exports.rhinoSchema_create_post  = async function(req, res) {
+    console.log(req.body)
+    let document = new rhino();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.rhino_species = req.body.rhino_species;
+    document.rhino_age = req.body.rhino_age;
+    document.endangerment_status = req.body.endangerment_status;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    } 
+   }
 // Handle Rhino delete from on DELETE.
 exports.rhinoSchema_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: Rhino delete DELETE ' + req.params.id);

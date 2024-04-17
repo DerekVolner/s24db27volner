a4ff9e821abb1da95e2133cfd0aa8da584b05b26
Derek Volner <89471429+DerekVolner@users.noreply.github.com>
2024-04-17 16:10:57 -0500

    Assignment 13 part 6 complete

--- models/rhinoSchema.js
+++ models/rhinoSchema.js
@@ -2,7 +2,14 @@ const mongoose = require("mongoose")
 const rhinoSchema = mongoose.Schema({
 rhino_species: String,
 endangerment_status: String,
-rhino_age: Number
+rhino_age: {
+    type: Number,
+    min: [0,'Rhino too young'],
+    max: [57, 'Rhino is too old, and older then current oldest recorded Rhino Fausta']
+}
+
+
+
 })
 module.exports = mongoose.model("Rhino", 
 rhinoSchema)
